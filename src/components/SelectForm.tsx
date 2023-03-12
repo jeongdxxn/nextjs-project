9b065a8bbec50feddaa8777d0b66efaa24a996d7
jeongdxxn <dxxn.jeong@gmail.com>
2023-03-12 21:06:52 +0900

    select form

---
+++ src/components/SelectForm.tsx
@@ -0,0 +1,129 @@
+import { Fragment, useState } from "react";
+import { people } from "../data/userData";
+import { Listbox, Transition } from "@headlessui/react";
+import { CheckIcon, ChevronUpDownIcon } from "@heroicons/react/20/solid";
+import Image from "next/image";
+
+interface SelectFormProps {
+  id: number;
+  name: string;
+  avatar: string;
+}
+export default function SelectForm() {
+  const [selected, setSelected] = useState(people[3]);
+  function classNames(...classes: any) {
+    return classes.filter(Boolean).join(" ");
+  }
+  return (
+    <div className="flex justify-center items-center flex-col p-20">
+      {/* select - option 사용 */}
+      {/* <select
+          onChange={(e) => {
+            setSelected(e.target.value);
+          }}
+        >
+          <label>select</label>
+          <option value={1}>one</option>
+          <option value={2}>two</option>
+          <option value={3}>three</option>
+          <option value={4}>four</option>
+          <option value={5}>fiv</option>
+        </select> */}
+
+      <Listbox value={selected} onChange={setSelected}>
+        {({ open }) => (
+          <>
+            {/* label */}
+            <Listbox.Label className="block text-sm font-medium leading-6 text-gray-900">
+              Assigned to
+            </Listbox.Label>
+            {/* selectbox */}
+            <div className="relative mt-2">
+              <Listbox.Button className="relative w-80 cursor-default rounded-md bg-white py-1.5 pl-3 pr-10 text-left text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:outline-none focus:ring-2 focus:ring-indigo-500 sm:text-sm sm:leading-6">
+                <span className="flex items-center">
+                  <Image
+                    width={20}
+                    height={20}
+                    src={selected.avatar}
+                    alt=""
+                    className="flex-shrink-0 rounded-full"
+                  />
+                  <span className="ml-3 block truncate">{selected.name}</span>
+                </span>
+                <span className="pointer-events-none absolute inset-y-0 right-0 ml-3 flex items-center pr-2">
+                  <ChevronUpDownIcon
+                    className="h-5 w-5 text-gray-400"
+                    aria-hidden="true"
+                  />
+                </span>
+              </Listbox.Button>
+
+              <Transition
+                show={open}
+                as={Fragment}
+                leave="transition ease-in duration-100"
+                leaveFrom="opacity-100"
+                leaveTo="opacity-0"
+              >
+                <Listbox.Options className="absolute z-10 mt-1 max-h-56 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm">
+                  {people.map((person) => (
+                    <Listbox.Option
+                      key={person.id}
+                      className={({ active }) =>
+                        classNames(
+                          active ? "bg-indigo-600 text-white" : "text-gray-900",
+                          "relative cursor-default select-none py-2 pl-3 pr-9"
+                        )
+                      }
+                      value={person}
+                    >
+                      {({ selected, active }) => (
+                        <>
+                          <div className="flex items-center">
+                            <Image
+                              width={20}
+                              height={20}
+                              src={person.avatar}
+                              alt=""
+                              className="flex-shrink-0 rounded-full"
+                            />
+                            <span
+                              className={classNames(
+                                selected ? "font-semibold" : "font-normal",
+                                "ml-3 block truncate"
+                              )}
+                            >
+                              {person.name}
+                            </span>
+                          </div>
+
+                          {selected ? (
+                            <span
+                              className={classNames(
+                                active ? "text-white" : "text-indigo-600",
+                                "absolute inset-y-0 right-0 flex items-center pr-4"
+                              )}
+                            >
+                              <CheckIcon
+                                className="h-5 w-5"
+                                aria-hidden="true"
+                              />
+                            </span>
+                          ) : null}
+                        </>
+                      )}
+                    </Listbox.Option>
+                  ))}
+                </Listbox.Options>
+              </Transition>
+            </div>
+          </>
+        )}
+      </Listbox>
+      {/* 
+        <div>
+          <span>selected</span> <span>{selected}</span>
+        </div> */}
+    </div>
+  );
+}
